edb6046ab33bfff05145d1336dfeb7de1ab1fbdf
Maxime Payant <maxime@pop-os.localdomain>
2021-01-11 14:44:34 +0100

    True koala

---
+++ hospital/SickKoala.cpp
@@ -0,0 +1,66 @@
+/*
+** ShipWreck engine, 2020
+** SickKoala.cpp
+*/
+
+#include "SickKoala.hpp"
+
+SickKoala::SickKoala(const std::string& name) :
+m_name(name)
+{
+}
+
+SickKoala::~SickKoala()
+{
+    std::cout
+        << "Mr.["
+        << m_name
+        << "]: Kreooogg!! I'm cuuuured!"
+        << std::endl;
+}
+
+void SickKoala::poke() const
+{
+    std::cout
+        << "Mr.["
+        << m_name
+        << "]: Gooeeeeerrk!!"
+        << std::endl;
+}
+
+bool SickKoala::takeDrug(const std::string& drug) const
+{
+    if (drug == "Mars")
+        std::cout
+            << "Mr.["
+            << m_name
+            << "]: Mars, and it kreogs!"
+            << std::endl;
+    else if (drug == "Kinder")
+        std::cout
+            << "Mr.["
+            << m_name
+            << "]: There is a toy inside!"
+            << std::endl;
+    else {
+        std::cout
+            << "Mr.["
+            << m_name
+            << "]: Goerkreog!"
+            << std::endl;
+        return (false);
+    }
+    return (true);
+}
+
+void SickKoala::overDrive(std::string str) const
+{
+    for (std::size_t check = 0; (check = str.find("Kreog", check)) != std::string::npos && check < str.size(); check += 1)
+        str = str.replace(check, 5, "1337");
+    std::cout
+            << "Mr.["
+            << m_name
+            << "]: "
+            << str
+            << std::endl;
+}
---
+++ hospital/SickKoala.hpp
@@ -0,0 +1,31 @@
+/*
+** ShipWreck engine, 2020
+** SickKoala.hpp
+*/
+
+#ifndef SICK_KOALA_HPP
+#define SICK_KOALA_HPP
+
+#include <iostream>
+#include <string>
+
+class SickKoala
+{
+
+    private:
+        std::string m_name;
+
+    public:
+        SickKoala() = delete;
+        SickKoala(SickKoala const& ) = delete;
+        SickKoala(SickKoala&& ) = delete;
+        SickKoala(const std::string& name);
+        ~SickKoala();
+
+        void poke() const;
+        bool takeDrug(const std::string& drug) const;
+        void overDrive(std::string str) const;
+
+}; // class SickKoala
+
+#endif // SICK_KOALA_HPP
